ef5e2bfff45ded8b720e73ca5dacb4875e15a827
Jameson Nuss <jamesonnuss@gmail.com>
2022-04-19 13:48:13 -0700

    plaid pantry test

--- web/wp-content/themes/rwesttheme/webpack.config.js
+++ web/wp-content/themes/rwesttheme/webpack.config.js
@@ -2,101 +2,102 @@
  * When passed a string, Glob will attempt to find each file that matches the
  * path given and return each path to the file as string[]
  */
-const glob = require("glob");
+ const glob = require('glob')
 
-/**
- * The Path API will be used to get the absolute path to the directory where we
- * plan to run Webpack
- */
-const path = require("path");
+ /**
+  * The Path API will be used to get the absolute path to the directory where we
+  * plan to run Webpack
+  */
+ const path = require('path')
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
-/* ---------------
+ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+ /* ---------------
  * Main config
  * We will place here all the common settings
  * ---------------*/
 var config = {
-  devtool: "source-map",
+  
+  devtool: 'source-map',
   externals: {
-    jquery: "jQuery",
+    jquery: 'jQuery',
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "",
-            },
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true, // <-- !!IMPORTANT!!
-            },
-          },
-
-          {
-            loader: "resolve-url-loader",
-            options: {
-              root: "",
-              sourceMap: true, // <-- !!IMPORTANT!!
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true, // <-- !!IMPORTANT!!
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
-        generator: {
-          filename: "img/[name][ext]",
-          emit: false,
-        },
-        use: {
-          loader: "file-loader",
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"]
+    },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
           options: {
-            name: "[name].[ext]",
-            outputPath: "./img/",
+            publicPath: '',
           },
         },
-      },
-      {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][ext]",
-          emit: false,
-        },
-        exclude: [/node_modules/],
-        use: {
-          loader: "file-loader",
+        {
+          loader: "css-loader",
           options: {
-            name: "[name].[ext]",
-            outputPath: "./fonts/",
-          },
+            sourceMap: true, // <-- !!IMPORTANT!!
+          }
+        },
+        
+        {
+          loader: 'resolve-url-loader',
+          options: {
+            root: '',
+            sourceMap: true, // <-- !!IMPORTANT!!
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true, // <-- !!IMPORTANT!!
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
+      generator: {
+        filename: 'img/[name][ext]',
+        emit: false,
+      },
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: "./img/"
         },
       },
-    ],
+    },
+    {
+      test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      type: "asset/resource",
+      generator: {
+        filename: 'fonts/[name][ext]',
+        emit: false,
+      },
+      exclude: [ /node_modules/ ],
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: "./fonts/"
+        }
+      }
+    }
+    ]
   },
 };
 
 var configMain = Object.assign({}, config, {
   name: "configMain",
-  entry: path.resolve(process.cwd(), "src", "index.js"),
+  entry: path.resolve( process.cwd(), 'src', 'index.js' ),
   output: {
     path: __dirname + "/build",
     publicPath: "/",
@@ -104,17 +105,17 @@ var configMain = Object.assign({}, config, {
     // clean: true,
   },
   plugins: [
-    new RemoveEmptyScriptsPlugin(),
-    new CleanWebpackPlugin(),
+    // new RemoveEmptyScriptsPlugin(),
+    // new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "main.css",
-    }),
-  ],
+      filename: "main.css"
+    })
+  ]
 });
 
 var configEditor = Object.assign({}, config, {
   name: "configEditor",
-  entry: path.resolve(process.cwd(), "src", "editor.js"),
+  entry: path.resolve( process.cwd(), 'src', 'editor.js' ),
   output: {
     path: __dirname + "/build",
     publicPath: "/",
@@ -125,18 +126,15 @@ var configEditor = Object.assign({}, config, {
     new RemoveEmptyScriptsPlugin(),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "editor.css",
-    }),
-  ],
+      filename: "editor.css"
+    })
+  ]
 });
 
 var configBlocks = Object.assign({}, config, {
   name: "configBlocks",
-  entry: glob.sync("./src/blocks/**.scss").reduce((acc, path) => {
-    const entry = path.substring(
-      path.lastIndexOf("_") + 1,
-      path.lastIndexOf(".")
-    );
+  entry: glob.sync('./src/blocks/**.scss').reduce((acc, path) => {
+    const entry =  path.substring(path.lastIndexOf("_") + 1, path.lastIndexOf("."));
     acc[entry] = path;
     return acc;
   }, {}),
@@ -144,13 +142,13 @@ var configBlocks = Object.assign({}, config, {
     path: __dirname + "/template-parts",
     publicPath: "/",
     // filename: "js/app.admin.min.js"
-    filename: "./blocks/[name]/[name].js",
+    filename: './blocks/[name]/[name].js',
   },
   plugins: [
-    new RemoveEmptyScriptsPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "./blocks/[name]/[name].css",
-    }),
-  ],
+    new RemoveEmptyScriptsPlugin(), 
+      new MiniCssExtractPlugin({
+          filename: './blocks/[name]/[name].css',
+      })
+  ]
 });
-module.exports = [configMain, configBlocks, configEditor];
+ module.exports = [configMain, configBlocks, configEditor];
